251c9a8cb37c23a23a73bac989204f24a1a0c8bf
ARD11111 <deshmukhaditi221104@gmail.com>
2025-08-16 09:14:18 +0000

    Add customize grid and clear grid functions

--- skecthpad.js
+++ skecthpad.js
@@ -1,33 +1,76 @@
 const container = document.getElementById("container");
-container.setAttribute("style", "display:flex; flex-wrap:wrap; justify-content:center; margin:30px 20px; height:900px;");
+container.setAttribute("style", "display:flex; flex-wrap:wrap; justify-content:center; margin:30px 20px; height:860px;");
 
+let noOfSquares = 16;
+let box;
+// div creation loop
 
+function createGrid(noOfSquares) {
+  for (let i = 0; i < noOfSquares; i++) {
 
-for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < noOfSquares; j++) {
+      box = document.createElement("div");
+      box.setAttribute("style", `width:calc(100%/ ${noOfSquares}); height:calc(100%/ ${noOfSquares}); aspect-ratio: 1/1;  box-sizing:border-box; `);
+      box.style.border = "1px solid green";
+      box.className = "old";
+      container.appendChild(box);
 
-  for (let j = 0; j < 16; j++) {
-    let box = document.createElement("div");
-    box.setAttribute("style", " width:calc(100%/16); height:calc(100%/16); aspect-ratio: 1/1; border: 2px solid blue; box-sizing:border-box;");
-    box.innerHTML=`${j}`;
-    // box.className="box";
-    container.appendChild(box);
+    }
 
-
-  
   }
 
 }
+createGrid(noOfSquares); // create intial 16X16 grid
+
+//remove old grid
+function removeGrid(noOfSquares) {
+      let temp = document.querySelectorAll(".old");
+      temp.forEach(element => {
+        element.remove();
+      }
+
+);
+}
+
+
+ 
+
 
 
 container.addEventListener("mouseover", (e) => {
-  if(e.target.tagName === "DIV" && e.target !== container ){
-    e.target.style.backgroundColor="black";
+  if (e.target.tagName === "DIV" && e.target !== container) {
+    e.target.style.backgroundColor = "black";
   }
-  
+
 });
 container.addEventListener("click", (e) => {
-  if(e.target.tagName === "DIV" && e.target !== container ){
-    e.target.style.backgroundColor="white";
+  if (e.target.tagName === "DIV" && e.target !== container) {
+    e.target.style.backgroundColor = "white";
   }
+
+});
+const customize = document.querySelector("#customize");
+customize.addEventListener("click", () => {
+  removeGrid(noOfSquares);
+  let newGrid = Number(prompt("Enter the no. fo squares per side for the new grid (less than 100)", "16"));
+ 
+  if(newGrid > 100){
+  newGrid = prompt("enter a number less than 100");
+ }
+  noOfSquares = Number(newGrid);
+ 
+ 
+
+  createGrid(noOfSquares);
+});
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click", () => {
+  let temp = document.querySelectorAll(".old");
+      temp.forEach(element => {
+        element.style.backgroundColor = "white";;
+      }
+    );
+
   
 });
